26f43ea25708b0b77457d0f230079cbac77347ce
ChenHao <275258836@qq.com>
2016-07-26 19:10:06 +0800

    正在操作编辑页面

--- dbmp/views/v_dbmp_mysql_instance.py
+++ dbmp/views/v_dbmp_mysql_instance.py
@@ -5,9 +5,10 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from common.util.pagination import Pagination
 from common.util.view_url_path import ViewUrlPath
-import simplejson as json
-# from dbmp.models.cmdb_os import CmdbOs
+from dbmp.models.cmdb_os import CmdbOs
 from dbmp.models.dbmp_mysql_instance import DbmpMysqlInstance
+
+import simplejson as json
 
 # Create your views here.
 
@@ -16,6 +17,12 @@ def home(request):
 
 def index(request):
     params = {}
+    params['message'] = {
+        'code': request.session.get('alert_code', ''),
+        'msg': request.session.get('alert_msg', '')
+    }
+    request.session['alert_code'] = ''
+    request.session['alert_msg'] = ''
 
     try:  
         cur_page = int(request.GET.get('cur_page', '1'))  
@@ -43,19 +50,57 @@ def view(request):
     pass
 
 def edit(request):
-    pass
+    params = {}
+    params['message'] = {
+        'code': request.session.get('alert_code', ''),
+        'msg': request.session.get('alert_msg', '')
+    }
+    request.session['alert_code'] = ''
+    request.session['alert_msg'] = ''
+
+    # 点击链接转跳到编辑页面
+    if request.method == 'GET':
+        # 获取MySQL实例ID
+        mysql_instance_id = int(request.GET.get('mysql_instance_id', '0'))  
+
+        if mysql_instance_id:
+            # 获取MySQL实例
+            mysql_instance = DbmpMysqlInstance.objects.get(
+                                 mysql_instance_id = mysql_instance_id)
+            
+            params['mysql_instance'] = mysql_instance
+
+            if mysql_instance:
+                cmdb_os = CmdbOs.objects.get(os_id = mysql_instance.os_id)
+                params['cmdb_os'] = cmdb_os
+
+                # 如果MySQL实例没有指定OS则告警
+                if not cmdb_os:
+                    params['message']['code'] = 'warning'
+                    params['message']['msg'] = '该MySQL实例没有指定一个OS, 请绑定!'
+
+                    request.session['alert_code'] = 'warning'
+                    request.session['alert_msg'] = '对不起! 找不到指定的MySQL实例!'
+
+                return render(request, 'dbmp_mysql_instance/edit.html', params)
+            else:
+                # 返回点击编辑页面
+                request.session['alert_code'] = 'warning'
+                request.session['alert_msg'] = '对不起! 找不到指定的MySQL实例!'
+                return HttpResponseRedirect(request.environ['HTTP_REFERER'])
+        else:
+            request.session['alert_code'] = 'warning'
+            request.session['alert_msg'] = '对不起! 找不到指定的MySQL实例!'
+            # 返回点击编辑页面
+            return HttpResponseRedirect(request.environ['HTTP_REFERER'])
+
+
+    # 点击保存转跳到 View 页面
+    if request.method == 'POST':
+        pass
 
 def delete(request):
-    try:  
-        cur_page = int(request.GET.get('cur_page', '1'))  
-        mysql_instance_id = int(request.GET.get('mysql_instance_id', '0'))  
-    except ValueError:  
-        cur_page = 1  
-
-    redirect_url = '{path}/index?cur_page={cur_page}'.format(
-                   path = ViewUrlPath.path_dbmp_mysql_instance(),
-                   cur_page = cur_page)
-    return HttpResponseRedirect(redirect_url)
+    pass
 
 def ajax_delete(request):
     """ajax 的方式删除MySQL实例"""
