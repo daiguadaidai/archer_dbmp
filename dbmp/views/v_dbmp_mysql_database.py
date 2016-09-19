06124aec8cc69dc5b79d995cb4f87206b2e52960
ChenHao <275258836@qq.com>
2016-09-19 19:04:47 +0800

    完成手动同步数据库

--- dbmp/views/v_dbmp_mysql_database.py
+++ dbmp/views/v_dbmp_mysql_database.py
@@ -19,9 +19,15 @@ logger = logging.getLogger('default')
 # Create your views here.
 
 def ajax_sync_database(request):
-    """ajax 同步更新当前实例最新数据库列表"""
+    """ajax 同步更新当前实例最新数据库列表
+    Return:
+        is_ok 0/1/2
+            0: 获取信息出错
+            1: 同步成功
+            2: 不用同步
+    """
 
-    is_ok = False
+    is_ok = 0
     if request.method == 'POST':
         mysql_instance_id = int(request.POST.get('mysql_instance_id', '0'))  
 
@@ -68,25 +74,28 @@ def ajax_sync_database(request):
         # 构造 list 对象
         dbmp_mysql_database_list = [DbmpMysqlDatabase(mysql_instance_id=mysql_instance_id, name=name) for name in database_names_lack]
         
-        print dbmp_mysql_database_list
-            
         # 获得多余过期的数据库名
         database_names_redundant = list(database_names_old - database_names_real)
 
         try:
             with transaction.atomic():
+
                 # 删除数据多余过期数据
                 if database_names_redundant:
                     DbmpMysqlDatabase.objects.filter(
                                 mysql_instance_id = mysql_instance_id).filter(
                                 name__in = database_names_redundant).delete()
+                    is_ok = 1
+            
 
                 # 插入缺少的数据库名
                 if dbmp_mysql_database_list:
                     DbmpMysqlDatabase.objects.bulk_create(dbmp_mysql_database_list)
+                    is_ok = 1
 
-            # 同步数据库名称成功
-            is_ok = True
+                # 不需要更新
+                if not database_names_redundant and not dbmp_mysql_database_list:
+                    is_ok = 2
         except IntegrityError, e:
             logger.info(traceback.format_exc())
             logger.info('添加失败')
@@ -105,28 +114,27 @@ def ajax_search_database(request):
 
     if request.method == 'POST':
         mysql_instance_id = int(request.POST.get('mysql_instance_id', '0'))  
-        if mysql_instance_id:
-            try:
-                with transaction.atomic():
-                    DbmpMysqlInstance.objects.filter(
-                                mysql_instance_id = mysql_instance_id).delete()
-                    logger.info('delete DbmpMysqlInstance')
-                    DbmpMysqlInstanceInfo.objects.filter(
-                                mysql_instance_id = mysql_instance_id).delete()
-                    logger.info('delete DbmpMysqlInstanceInfo')
-                    DbmpMysqlBackupInstance.objects.filter(
-                                mysql_instance_id = mysql_instance_id).delete()
-                    logger.info('delete DbmpMysqlBackupInfo')
-                    DbmpMysqlBackupInfo.objects.filter(
-                                mysql_instance_id = mysql_instance_id).delete()
-                    logger.info('delete DbmpMysqlBackupInfo')
-                    DbmpMysqlBackupRemote.objects.filter(
-                                mysql_instance_id = mysql_instance_id).delete()
-                    logger.info('delete DbmpMysqlBackupInfo')
 
-                is_delete = True
-            except Exception, e:
-                logger.info(traceback.format_exc())
+        if not mysql_instance_id: # 没有传入有效ID返回失败
+            logger.info(traceback.format_exc())
+            logger.info('传入DbmpMysqlInstance ID 无效: {id}'.format(id=mysql_instance_id))
+            respons_data = json.dumps(False)
+            return HttpResponse(respons_data, content_type='application/json')
 
-    respons_data = json.dumps(is_delete)
-    return HttpResponse(respons_data, content_type='application/json')
+        try:
+            dbmp_mysql_databases = DbmpMysqlDatabase.objects.values(
+                                                   'mysql_database_id',
+                                                   'update_time',
+                                                   'name').filter(
+                                mysql_instance_id = mysql_instance_id)
+            for dbmp_mysql_database in dbmp_mysql_databases:
+                dbmp_mysql_database['update_time'] = str(dbmp_mysql_database['update_time'])
+                print dbmp_mysql_database
+
+            respons_data = json.dumps(list(dbmp_mysql_databases))
+            return HttpResponse(respons_data, content_type='application/json')
+        except: # 没有获取到实例信息则转跳列表页面
+            respons_data = json.dumps(False)
+            return HttpResponse(respons_data, content_type='application/json')
+            logger.error(traceback.format_exc())
+            logger.error('获取DbmpMysqlDatabase失败')
