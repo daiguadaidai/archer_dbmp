44286112b7dde640832b3090e92498bade0a3fd8
ChenHao <275258836@qq.com>
2016-09-25 11:22:45 +0800

    实现执行原生SQL

--- dbmp/views/v_dbmp_mysql_business_detail.py
+++ dbmp/views/v_dbmp_mysql_business_detail.py
@@ -13,6 +13,8 @@ from dbmp.models.dbmp_mysql_instance import DbmpMysqlInstance
 from dbmp.models.dbmp_mysql_business import DbmpMysqlBusiness
 from dbmp.models.dbmp_mysql_business_detail import DbmpMysqlBusinessDetail
 from dbmp.models.dbmp_mysql_database import DbmpMysqlDatabase
+from dbmp.views.sql_handler.s_dbmp_mysql_business_detail import SQLDbmpMysqlBusinessDetail
+
 
 import simplejson as json
 import traceback
@@ -25,6 +27,8 @@ logger = logging.getLogger('default')
 @DecoratorTool.get_request_alert_message
 def index(request):
     params = {}
+    sql_business_detail_handler = SQLDbmpMysqlBusinessDetail()
+    sql_business_detail_handler.get_business_detail_index(1)
     return render(request, 'dbmp_mysql_business_detail/index.html', params)
 
 @DecoratorTool.get_request_alert_message
